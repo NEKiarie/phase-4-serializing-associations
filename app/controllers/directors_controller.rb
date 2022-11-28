d86ca497c552393f6864ab2285578c23fe2b431b
Elvis <s49188705@gmail.com>
2022-11-28 17:48:56 +0300

    Serializing-Associations

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,20 +1,21 @@
 class DirectorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
+ 
+   def index
     directors = Director.all
-    render json: directors
-  end
+    render json: directors, include: ['movies', 'movies.reviews']
+   end
 
-  def show
+   def show
     director = Director.find(params[:id])
-    render json: director
-  end
+    render json: director, include: ['movies', 'movies.reviews']
+   end
 
-  private
+   private
 
-  def render_not_found_response
+    def render_not_found_response
     render json: { error: "Director not found" }, status: :not_found
-  end
+   end
 
 end
